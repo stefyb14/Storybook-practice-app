b50eca8b943ec046a30efea6132dfe8a6bddd8db
Isabel <isagm@Ana-Isabel-Gadalas-MacBook-Pro.local>
2015-04-14 22:22:47 -0500

    Add chapters model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150415003417) do
+ActiveRecord::Schema.define(version: 20150415031853) do
+
+  create_table "chapters", force: true do |t|
+    t.string   "title"
+    t.integer  "rating"
+    t.text     "youtubelink"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
